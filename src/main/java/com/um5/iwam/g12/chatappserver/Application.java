f34db99c6993b8f0d3dff077a5fd44eabff31481
oualid <o-oualid@outlook.com>
2023-04-13 16:03:13 +0000

    rename the project and fix docker compose

--- src/main/java/com/um5/iwam/g12/chatappserver/Application.java
+++ src/main/java/com/um5/iwam/g12/chatappserver/Application.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
-public class ChatAppServerApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(ChatAppServerApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
